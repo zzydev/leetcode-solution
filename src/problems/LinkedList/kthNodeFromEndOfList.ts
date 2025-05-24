d35c228fb83fedeef5facb2d7242d66d9896ab17
zzydev <4amzzydev@gmail.com>
2025-05-25 00:01:05 +0800

    feat: add a solution for #面试题 02.02. Kth Node From End of List

---
+++ src/problems/LinkedList/kthNodeFromEndOfList.ts
@@ -0,0 +1,16 @@
+import { ListNode } from '@/utils/ListNode';
+
+export function kthToLast(head: ListNode | null, k: number): number {
+    let dummyHead = new ListNode();
+    dummyHead.next = head;
+    let slow: ListNode | null = dummyHead;
+    let fast: ListNode | null = dummyHead;
+    while (k-- && fast !== null) {
+      fast = fast.next;
+    }
+    while (fast !== null) {
+      fast = fast.next;
+      slow = slow!.next;
+    }
+    return slow!.val;
+};
