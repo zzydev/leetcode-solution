79efe7143e0571e9266688b8aed70f4cb5d0d9a6
zzydev <4amzzydev@gmail.com>
2025-05-24 16:38:28 +0800

    feat: add a solution for #206 Reverse Linked List

---
+++ src/problems/LinkedList/reverseLinkedList.ts
@@ -0,0 +1,13 @@
+import type { ListNode } from '@/utils/ListNode';
+
+export function reverseList(head: ListNode | null): ListNode | null {
+  let newHead: ListNode | null = null;
+  let p = head;
+  while (p != null) {
+    const tmp = p.next;
+    p.next = newHead;
+    newHead = p;
+    p = tmp;
+  }
+  return newHead;
+};
