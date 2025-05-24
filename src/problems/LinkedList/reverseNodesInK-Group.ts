0bf7859f29c53659663bcf3a4bc6ccae6013b5dc
zzydev <4amzzydev@gmail.com>
2025-05-24 20:31:23 +0800

    feat: add a solution for #25. Reverse Nodes in k-Group

---
+++ src/problems/LinkedList/reverseNodesInK-Group.ts
@@ -0,0 +1,46 @@
+import { ListNode } from '@/utils/ListNode';
+
+export function reverseKGroup(head: ListNode | null, k: number): ListNode | null {
+  const dummyHead = new ListNode();
+  let tail = dummyHead;
+
+  let p = head;
+  while (p !== null) {
+    let count = 0;
+    let q: ListNode | null = p;
+    while (q != null) {
+      count++;
+      if (count === k) {
+        break;
+      }
+      q = q.next;
+    }
+    if (q == null) {
+      tail.next = p;
+      return dummyHead.next;
+    }
+    else {
+      const tmp = q.next;
+      const group = reverse(p, q);
+      tail.next = group[0];
+      tail = group[1];
+      p = tmp;
+    }
+  }
+  return dummyHead.next;
+};
+
+function reverse(head: ListNode, tail: ListNode) {
+  let newHead: ListNode | null = null;
+  let p: ListNode | null = head;
+  while (p !== tail) {
+    const tmp: ListNode | null = p!.next;
+    p!.next = newHead;
+    newHead = p;
+    p = tmp;
+  }
+  tail.next = newHead;
+  newHead = tail;
+
+  return [tail, head];
+}
