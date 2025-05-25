692322022db361d078450a0a4aaada5a849cbd57
zzydev <4amzzydev@gmail.com>
2025-05-25 18:27:49 +0800

    feat: add a solution for #160. Intersection of Two Linked Lists

--- src/problems/LinkedList/intersectionOfTwoLinkedLists.ts
+++ src/problems/LinkedList/intersectionOfTwoLinkedLists.ts
@@ -1,32 +1,32 @@
-import { ListNode } from '@/utils/ListNode';
-
+import type { ListNode } from '@/utils/ListNode';
 
 export function getIntersectionNode(headA: ListNode | null, headB: ListNode | null): ListNode | null {
   let lenA = 0;
   let pA = headA;
   while (pA !== null) {
-    lenA ++;
+    lenA++;
     pA = pA.next;
   }
   let lenB = 0;
   let pB = headB;
   while (pB !== null) {
-    lenB ++;
+    lenB++;
     pB = pB.next;
   }
   pA = headA;
   pB = headB;
   if (lenA > lenB) {
-    for (let i = 0; i < lenA - lenB; i ++) {
+    for (let i = 0; i < lenA - lenB; i++) {
       pA = pA!.next;
     }
-  } else {
-    for (let i = 0; i < lenB - lenA; i ++) {
+  }
+  else {
+    for (let i = 0; i < lenB - lenA; i++) {
       pB = pB!.next;
     }
   }
 
-  while (pA != pB) {
+  while (pA !== pB) {
     pA = pA!.next;
     pB = pB!.next;
   }
