d458bbe4d28fceda0c0555020cc187e7190c24a2
zzydev <4amzzydev@gmail.com>
2025-05-28 20:13:14 +0800

    feat: add a solution for #222. Count Complete Tree Nodes

---
+++ src/problems/Tree/countCompleteTreeNodes.ts
@@ -0,0 +1,6 @@
+import { TreeNode } from "@/utils/TreeNode";
+
+export function countNodes(root: TreeNode | null): number {
+    if (root === null) return 0;
+    return countNodes(root.left) + countNodes(root.right) + 1;
+};
