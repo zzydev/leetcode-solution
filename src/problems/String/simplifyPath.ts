89b4b4f59f71f377bb36dc82522eac4347e735ca
zzydev <4amzzydev@gmail.com>
2025-05-29 18:02:21 +0800

    feat: add a solution for #71. Simplify Path

---
+++ src/problems/String/simplifyPath.ts
@@ -0,0 +1,31 @@
+export function simplifyPath(path: string): string {
+  let res = '';
+  let name = '';
+  if (path.charAt(path.length - 1) !== '/') {
+    path += '/';
+  }
+
+  for (const c of path) {
+    if (c !== '/') {
+      name += c;
+    }
+    else {
+      if (name === '..') {
+        while (res.length > 0 && res.charAt(res.length - 1) !== '/') {
+          res = res.slice(0, -1);
+        }
+        if (res.length > 0) {
+          res = res.slice(0, -1);
+        }
+      }
+      else if (name !== '.' && name !== '') {
+        res += `/${name}`;
+      }
+      name = '';
+    }
+  }
+  if (res === '') {
+    res = '/';
+  }
+  return res;
+}
