aaa5a4988b2e67c566e7c3d2e48b49d11db86cc9
zzydev <4amzzydev@gmail.com>
2025-05-29 18:03:09 +0800

    feat: add a solution for #113. Path Sum II

---
+++ src/problems/Tree/pathSumIi.ts
@@ -0,0 +1,36 @@
+import type { TreeNode } from '@/utils/TreeNode';
+
+export function pathSum(root: TreeNode | null, targetSum: number): number[][] {
+  const res: number[][] = [];
+  const path: number[] = [];
+  if (root === null) {
+    return res;
+  }
+  path.push(root.val);
+  dfs(root, targetSum - root.val);
+  return res;
+
+  function dfs(root: TreeNode, sum: number) {
+    if (root.left === null && root.right === null && sum === 0) {
+      res.push([...path]);
+      return;
+    }
+    if (root.left === null && root.right === null) {
+      return;
+    }
+    if (root.left !== null) {
+      path.push(root.left.val);
+      sum -= root.left.val;
+      dfs(root.left, sum);
+      sum += root.left.val;
+      path.pop();
+    }
+    if (root.right !== null) {
+      path.push(root.right.val);
+      sum -= root.right.val;
+      dfs(root.right, sum);
+      sum += root.right.val;
+      path.pop();
+    }
+  }
+};
