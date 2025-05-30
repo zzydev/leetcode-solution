2d4e6b63f7f80a8a1ff954d05c7eb20bc25a5a61
zzydev <4amzzydev@gmail.com>
2025-05-30 16:08:10 +0800

    feat: add a solution for #101. Symmetric Tree

---
+++ src/problems/Tree/symmetricTree.ts
@@ -0,0 +1,25 @@
+import type { TreeNode } from '@/utils/TreeNode';
+
+export function isSymmetric(root: TreeNode | null): boolean {
+  if (root === null) {
+    return true;
+  }
+  return compare(root.left, root.right);
+  function compare(left: TreeNode | null, right: TreeNode | null): boolean {
+    if (left === null && right !== null) {
+      return false;
+    }
+    else if (left !== null && right === null) {
+      return false;
+    }
+    else if (left === null && right === null) {
+      return true;
+    }
+    else if (left!.val !== right!.val) {
+      return false;
+    }
+    else {
+      return compare(left!.left, right!.right) && compare(left!.right, right!.left);
+    }
+  }
+};
