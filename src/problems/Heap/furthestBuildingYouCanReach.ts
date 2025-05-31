3f0916d863b88aa6ebcb8f73f8cda9b2dd3e7120
zzydev <4amzzydev@gmail.com>
2025-05-31 23:28:37 +0800

    feat: add a solution for #1642. Furthest Building You Can Reach

---
+++ src/problems/Heap/furthestBuildingYouCanReach.ts
@@ -0,0 +1,32 @@
+import { MaxHeap } from '@/utils/Heap';
+
+export function furthestBuilding(heights: number[], bricks: number, ladders: number): number {
+  const maxHeap = new MaxHeap<number>();
+  let totalBricksUsed = 0;
+  let lastPos = 0;
+  let prevHeight = heights[0];
+  for (let i = 1; i < heights.length; i++) {
+    const curHeight = heights[i];
+    if (prevHeight > curHeight) {
+      lastPos = i;
+    }
+    else {
+      const delta = curHeight - prevHeight;
+      maxHeap.insert(delta);
+      totalBricksUsed += delta;
+      if (totalBricksUsed > bricks && ladders > 0) {
+        const largestDiff = maxHeap.extract()!;
+        totalBricksUsed -= largestDiff;
+        ladders--;
+      }
+      if (totalBricksUsed <= bricks) {
+        lastPos = i;
+      }
+      else {
+        break;
+      }
+    }
+    prevHeight = curHeight;
+  }
+  return lastPos;
+};
