16bcc1eb7ed78f36a1a5b0d804f6696ec227beaa
zzydev <4amzzydev@gmail.com>
2025-05-31 22:25:27 +0800

    feat: add a solution for #373. Find K Pairs with Smallest Sums

---
+++ src/problems/Heap/findKPairsWithSmallestSums.ts
@@ -0,0 +1,25 @@
+import { Heap } from '@/utils/Heap';
+
+export function kSmallestPairs(nums1: number[], nums2: number[], k: number): number[][] {
+  if (nums1.length === 0 || nums2.length === 0 || k <= 0) {
+    return [];
+  }
+  const minHeap = new Heap<[number, number]>((a, b) => {
+    return nums1[a[0]] + nums2[a[1]] < nums1[b[0]] + nums2[b[1]];
+  });
+
+  for (let i = 0; i < nums1.length; i++) {
+    minHeap.insert([i, 0]);
+  }
+
+  const res: number[][] = [];
+  for (let count = 0; count < k && !minHeap.isEmpty(); count++) {
+    const [i, j] = minHeap.extract()!;
+    res.push([nums1[i], nums2[j]]);
+
+    if (j + 1 < nums2.length) {
+      minHeap.insert([i, j + 1]);
+    }
+  }
+  return res;
+};
