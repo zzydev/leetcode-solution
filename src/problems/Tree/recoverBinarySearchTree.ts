7ae365df699d3ec488cf192a1d85616a2330053d
zzydev <4amzzydev@gmail.com>
2025-06-04 10:01:54 +0800

    feat: add a solution for #99. Recover Binary Search Tree

---
+++ src/problems/Tree/recoverBinarySearchTree.ts
@@ -0,0 +1,33 @@
+import type { TreeNode } from '@/utils/TreeNode';
+
+export function recoverTree(root: TreeNode | null): void {
+  let first: TreeNode | null = null;
+  let second: TreeNode | null = null;
+  let pre: TreeNode | null = null;
+  inorderTraversal(root);
+  if (first !== null && second !== null) {
+    swap(first, second);
+  }
+
+  function swap(first: TreeNode, second: TreeNode) {
+    [first.val, second.val] = [second.val, first.val];
+  }
+
+  function check(cur: TreeNode) {
+    if (pre !== null && pre.val > cur.val) {
+      if (first === null) {
+        first = pre;
+      }
+      second = cur;
+    }
+    pre = cur;
+  }
+
+  function inorderTraversal(root: TreeNode | null) {
+    if (root !== null) {
+      inorderTraversal(root.left);
+      check(root);
+      inorderTraversal(root.right);
+    }
+  }
+};
