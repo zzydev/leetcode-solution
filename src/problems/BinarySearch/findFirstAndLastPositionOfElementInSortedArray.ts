c489d62635c7f60d3c7c49c95c230d60e9854fae
zzydev <4amzzydev@gmail.com>
2025-06-10 07:13:24 +0800

    chore: Optimize comparison operators and variable declarations in findFirstAndLastPositionOfElementInSortedArray.js

--- src/problems/BinarySearch/findFirstAndLastPositionOfElementInSortedArray.ts
+++ src/problems/BinarySearch/findFirstAndLastPositionOfElementInSortedArray.ts
@@ -1,12 +1,12 @@
 export function searchRange(nums: number[], target: number): number[] {
   const ans: number[] = [-1, -1];
-  if (nums == null || nums.length <= 0) {
+  if (nums === null || nums.length <= 0) {
     return ans;
   }
 
   const l = lowerBound(nums, target);
   const r = upperBound(nums, target);
-  if (l == r) {
+  if (l === r) {
     return ans;
   }
   ans[0] = l;
@@ -15,7 +15,8 @@ export function searchRange(nums: number[], target: number): number[] {
 }
 
 function lowerBound(nums: number[], target: number) {
-  let l: number = 0; let r = nums.length;
+  let l: number = 0;
+  let r = nums.length;
   while (l < r) {
     const m = l + ((r - l) >> 1);
     if (nums[m] < target) {
@@ -29,7 +30,8 @@ function lowerBound(nums: number[], target: number) {
 }
 
 function upperBound(nums: number[], target: number) {
-  let l: number = 0; let r = nums.length;
+  let l: number = 0;
+  let r = nums.length;
   while (l < r) {
     const m = l + ((r - l) >> 1);
     if (nums[m] <= target) {
