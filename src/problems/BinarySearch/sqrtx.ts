8bdeb9f5b2c6bd475bccfc1c7bd87f24fc8dde1e
zzydev <4amzzydev@gmail.com>
2025-06-12 20:04:15 +0800

    feat: add a solution for #69. Sqrt(x)

---
+++ src/problems/BinarySearch/sqrtx.ts
@@ -0,0 +1,30 @@
+export function mySqrt(x: number): number {
+  let l = 0;
+  let r = x;
+  while (l < r) {
+    const m = l + ((r - l) >> 1);
+    const cmp = cmpSqrt(x, m);
+    if (cmp < 0) {
+      l = m + 1;
+    }
+    else {
+      r = m;
+    }
+  }
+  if (l * l === x) {
+    return l;
+  }
+  return l - 1;
+};
+
+function cmpSqrt(x: number, m: number): -1 | 0 | 1 {
+  if (m * m < x) {
+    return -1;
+  }
+  else if (m * m === x) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
+}
