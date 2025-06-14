16693ae138c6e8ec5b24df45d19375f492d40389
zzydev <4amzzydev@gmail.com>
2025-06-14 15:32:26 +0800

    feat：add a solution for #242. Valid Anagram

---
+++ src/problems/HashTable/validAnagram.ts
@@ -0,0 +1,20 @@
+export function isAnagram(s: string, t: string): boolean {
+  if (s.length !== t.length) {
+    return false;
+  }
+  const map_s = new Map();
+  const map_t = new Map();
+  for (const c of s) {
+    map_s.set(c, (map_s.get(c) as number || 0) + 1);
+  }
+  for (const c of t) {
+    map_t.set(c, (map_t.get(c) as number || 0) + 1);
+  }
+
+  for (const [key, value] of map_s.entries()) {
+    if (map_t.get(key) !== value) {
+      return false;
+    }
+  }
+  return true;
+};
