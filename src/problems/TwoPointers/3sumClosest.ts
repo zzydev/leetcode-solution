b23d5895af90f52f17a34672f3c3ac0ab29ba082
zzydev <4amzzydev@gmail.com>
2025-06-14 14:56:18 +0800

    feat: add a solution for #16. 3Sum Closest

---
+++ src/problems/TwoPointers/3sumClosest.ts
@@ -0,0 +1,24 @@
+export function threeSumClosest(nums: number[], target: number): number {
+  let ans = nums[0] + nums[1] + nums[2];
+  nums.sort((a, b) => a - b);
+  const len = nums.length;
+  for (let i = 0; i < len - 2; i++) {
+    let l = i + 1;
+    let r = len - 1;
+    while (l < r) {
+      if (Math.abs(nums[i] + nums[l] + nums[r] - target) < Math.abs(ans - target)) {
+        ans = nums[i] + nums[l] + nums[r];
+      }
+      if (nums[i] + nums[l] + nums[r] === target) {
+        return target;
+      }
+      else if (nums[i] + nums[l] + nums[r] > target) {
+        r--;
+      }
+      else {
+        l++;
+      }
+    }
+  }
+  return ans;
+};
