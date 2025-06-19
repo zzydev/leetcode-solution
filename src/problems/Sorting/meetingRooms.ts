96a1e42fe7457cc7b834195ed0fdd9c4b8109603
zzydev <4amzzydev@gmail.com>
2025-06-19 09:06:15 +0800

    feat: add a solution for #252. Meeting Rooms

---
+++ src/problems/Sorting/meetingRooms.ts
@@ -0,0 +1,9 @@
+export function canAttendMeetings(intervals: number[][]) {
+  intervals.sort((a, b) => a[0] - b[0]);
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] < intervals[i - 1][1]) {
+      return false;
+    }
+  }
+  return true;
+}
