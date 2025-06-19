ce604d80b3e60c156b3ffceb133cb62ec9376263
zzydev <4amzzydev@gmail.com>
2025-06-19 10:51:37 +0800

    chore: Update project configuration and documentation

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,14 +1,14 @@
-import { defineConfig } from "vitest/config";
-import path from "path";
+import path from 'node:path';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-    resolve: {
-        alias: {
-            "@": path.resolve(__dirname, "./src"),
-        },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'),
     },
-    test: {
-        include: ["**/__tests__/**/*.{js,ts}"],
-        testTimeout: 35000,
-    },
+  },
+  test: {
+    include: ['**/__tests__/**/*.{js,ts}'],
+    testTimeout: 35000,
+  },
 });
