0bf52f34e03c6601b3ecbe31a25e45c8231c802e
zzydev <4amzzydev@gmail.com>
2025-06-19 09:59:24 +0800

    feat: add a solution for #56. Merge Intervals

---
+++ src/problems/Sorting/mergeIntervals.ts
@@ -0,0 +1,20 @@
+export function merge(intervals: number[][]): number[][] {
+  intervals.sort((a, b) => a[0] - b[0]);
+  const res: number[][] = [];
+  let curL = intervals[0][0];
+  let curR = intervals[0][1];
+  for (let i = 1; i < intervals.length; i++) {
+    if (intervals[i][0] <= curR) {
+      if (intervals[i][1] >= curR) {
+        curR = intervals[i][1];
+      }
+    }
+    else {
+      res.push([curL, curR]);
+      curL = intervals[i][0];
+      curR = intervals[i][1];
+    }
+  }
+  res.push([curL, curR]);
+  return res;
+};
