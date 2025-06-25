67993b4097a99295d3f0506ca779ff99bcdf31be
zzydev <4amzzydev@gmail.com>
2025-06-25 23:06:48 +0800

    refactor: replace C-style loops with for-of #1122. Relative Sort Array and format with prettier

--- src/problems/HashTable/__tests__/relativeSortArray.spec.ts
+++ src/problems/HashTable/__tests__/relativeSortArray.spec.ts
@@ -13,7 +13,9 @@ describe('relativeSortArray', () => {
   it('should sort according to target order', () => {
     const arr1 = [2, 3, 1, 3, 2, 4, 6, 7, 9, 2, 19];
     const arr2 = [2, 1, 4, 3, 9, 6];
-    expect(relativeSortArray(arr1, arr2)).toEqual([2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19]);
+    expect(relativeSortArray(arr1, arr2)).toEqual([
+      2, 2, 2, 1, 4, 3, 3, 9, 6, 7, 19,
+    ]);
   });
 
   it('should handle elements not in arr2 at the end in ascending order', () => {
@@ -61,7 +63,9 @@ describe('relativeSortArray', () => {
   it('should handle large numbers', () => {
     const arr1 = [1000, 2000, 3000, 2000, 1000];
     const arr2 = [2000, 1000];
-    expect(relativeSortArray(arr1, arr2)).toEqual([2000, 2000, 1000, 1000, 3000]);
+    expect(relativeSortArray(arr1, arr2)).toEqual([
+      2000, 2000, 1000, 1000, 3000,
+    ]);
   });
 
   it('should handle arr1 and arr2 with all same elements but different order', () => {
@@ -73,7 +77,9 @@ describe('relativeSortArray', () => {
   it('should handle arr1 with elements not in arr2 and sort them at the end', () => {
     const arr1 = [2, 1, 2, 5, 7, 1, 9, 3, 6, 8, 8];
     const arr2 = [2, 1, 8, 3];
-    expect(relativeSortArray(arr1, arr2)).toEqual([2, 2, 1, 1, 8, 8, 3, 5, 6, 7, 9]);
+    expect(relativeSortArray(arr1, arr2)).toEqual([
+      2, 2, 1, 1, 8, 8, 3, 5, 6, 7, 9,
+    ]);
   });
 
   it('should handle arr1 with multiple elements not in arr2', () => {
--- src/problems/HashTable/relativeSortArray.ts
+++ src/problems/HashTable/relativeSortArray.ts
@@ -8,21 +8,21 @@ export function relativeSortArray(arr1: number[], arr2: number[]): number[] {
     if (!map.has(num)) {
       continue;
     }
-    map.set(num, (map.get(num)! + 1));
+    map.set(num, (map.get(num) ?? 0) + 1);
   }
   let k = 0;
-  for (let i = 0; i < arr2.length; i++) {
-    const cnt = map.get(arr2[i]) ?? 0;
+  for (const num of arr2) {
+    const cnt = map.get(num) ?? 0;
     for (let j = 0; j < cnt; j++) {
-      res[k + j] = arr2[i];
+      res[k + j] = num;
     }
     k += cnt;
   }
   arr1.sort((a, b) => a - b);
-  for (let i = 0; i < arr1.length; i++) {
-    if (!map.has(arr1[i])) {
-      res[k++] = arr1[i];
+  for (const num of arr1) {
+    if (!map.has(num)) {
+      res[k++] = num;
     }
   }
   return res;
-};
+}
