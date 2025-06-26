470f2bde76cb85fc9b5dfed3ae2a37dfca3f63f9
zzydev <4amzzydev@gmail.com>
2025-06-26 09:53:41 +0800

    style: format #8. String to Integer (atoi) with prettier

--- src/problems/String/stringToInteger.ts
+++ src/problems/String/stringToInteger.ts
@@ -6,30 +6,25 @@ export function myAtoi(s: string): number {
   const INT_MAX = 2 ** 31 - 1;
   const INT_MIN = -(2 ** 31);
   while (i < len && s[i] === ' ') i++;
-  if (i === len)
-    return 0;
+  if (i === len) return 0;
   if (i < len && s[i] === '+') {
     i++;
-  }
-  else if (i < len && s[i] === '-') {
+  } else if (i < len && s[i] === '-') {
     i++;
     minus = -minus;
   }
   while (i < len && isAlpha(s[i])) {
     const num = Number.parseInt(s[i]);
-    if (minus > 0 && res * 10 + num > INT_MAX)
-      return INT_MAX;
-    if (minus < 0 && -res * 10 - num < INT_MIN)
-      return INT_MIN;
+    if (minus > 0 && res * 10 + num > INT_MAX) return INT_MAX;
+    if (minus < 0 && -res * 10 - num < INT_MIN) return INT_MIN;
     res = res * 10 + num;
     i++;
   }
   res *= minus;
   return res;
-};
+}
 
 function isAlpha(str: string): boolean {
-  if (str >= '0' && str <= '9')
-    return true;
+  if (str >= '0' && str <= '9') return true;
   return false;
 }
