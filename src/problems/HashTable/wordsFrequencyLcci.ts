4609aaf964ff00ffc3d651acbb6c194e28d26440
zzydev <4amzzydev@gmail.com>
2025-06-26 08:04:33 +0800

    refactor: replace C-style loops with for-of #面试题 16.02. Words Frequency LCCI

--- src/problems/HashTable/wordsFrequencyLcci.ts
+++ src/problems/HashTable/wordsFrequencyLcci.ts
@@ -2,8 +2,8 @@ export class WordsFrequency {
   map;
   constructor(book: string[]) {
     this.map = new Map<string, number>();
-    for (let i = 0; i < book.length; i++) {
-      this.map.set(book[i], (this.map.get(book[i]) ?? 0) + 1);
+    for (const word of book) {
+      this.map.set(word, (this.map.get(word) ?? 0) + 1);
     }
   }
 
