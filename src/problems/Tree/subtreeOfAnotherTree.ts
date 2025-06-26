db1b8206381d545fc2ffb28c614fa552acef6ce4
zzydev <4amzzydev@gmail.com>
2025-06-26 10:07:40 +0800

    refactor: improve code style for #572. Subtree of Another Tree

--- src/problems/Tree/subtreeOfAnotherTree.ts
+++ src/problems/Tree/subtreeOfAnotherTree.ts
@@ -1,12 +1,19 @@
 import type { TreeNode } from '@/utils/TreeNode';
 import { isSameTree } from './sameTree';
 
-export function isSubtree(root: TreeNode | null, subRoot: TreeNode | null): boolean {
+export function isSubtree(
+  root: TreeNode | null,
+  subRoot: TreeNode | null,
+): boolean {
   if (root === subRoot || subRoot === null) {
     return true;
   }
   if (root === null) {
     return false;
   }
-  return isSameTree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
-};
+  return (
+    isSameTree(root, subRoot) ||
+    isSubtree(root.left, subRoot) ||
+    isSubtree(root.right, subRoot)
+  );
+}
