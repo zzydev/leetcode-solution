a8ddf7bbbd25c7c99eb5d58f873c45f6e260bfc5
zzydev <4amzzydev@gmail.com>
2025-06-26 09:32:56 +0800

    refactor: improve code style for 739. Daily Temperatures

--- src/problems/StackAndQueue/__tests__/dailyTemperatures.spec.ts
+++ src/problems/StackAndQueue/__tests__/dailyTemperatures.spec.ts
@@ -19,8 +19,9 @@ describe('dailyTemperatures', () => {
   });
 
   it('should solve basic case', () => {
-    expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73]))
-      .toEqual([1, 1, 4, 2, 1, 1, 0, 0]);
+    expect(dailyTemperatures([73, 74, 75, 71, 69, 72, 76, 73])).toEqual([
+      1, 1, 4, 2, 1, 1, 0, 0,
+    ]);
   });
 
   it('should handle constant temperatures', () => {
@@ -28,18 +29,20 @@ describe('dailyTemperatures', () => {
   });
 
   it('should handle mixed temperatures', () => {
-    expect(dailyTemperatures([55, 38, 53, 81, 61, 93, 97, 32, 43, 78]))
-      .toEqual([3, 1, 1, 2, 1, 1, 0, 1, 1, 0]);
+    expect(dailyTemperatures([55, 38, 53, 81, 61, 93, 97, 32, 43, 78])).toEqual(
+      [3, 1, 1, 2, 1, 1, 0, 1, 1, 0],
+    );
   });
 
   it('should handle large temperature range', () => {
-    const input = [...Array.from({ length: 100 }).keys()].map(i => i + 30);
+    const input = [...Array.from({ length: 100 }).keys()].map((i) => i + 30);
     const expected = [...Array.from({ length: 99 }).fill(1), 0];
     expect(dailyTemperatures(input)).toEqual(expected);
   });
 
   it('should handle random temperature fluctuations', () => {
-    expect(dailyTemperatures([34, 33, 32, 34, 33, 35, 36, 34, 33]))
-      .toEqual([5, 2, 1, 2, 1, 1, 0, 0, 0]);
+    expect(dailyTemperatures([34, 33, 32, 34, 33, 35, 36, 34, 33])).toEqual([
+      5, 2, 1, 2, 1, 1, 0, 0, 0,
+    ]);
   });
 });
--- src/problems/StackAndQueue/dailyTemperatures.ts
+++ src/problems/StackAndQueue/dailyTemperatures.ts
@@ -1,13 +1,16 @@
 export function dailyTemperatures(temperatures: number[]): number[] {
   const len = temperatures.length;
   const stack: number[] = [];
-  const res: number[] = new Array(len).fill(0);
+  const res: number[] = Array.from({ length: len }).fill(0) as number[];
   for (let i = 0; i < len; i++) {
-    while (stack.length !== 0 && temperatures[stack[stack.length - 1]] < temperatures[i]) {
-      const index: number = stack.pop()!;
+    while (
+      stack.length !== 0 &&
+      temperatures[stack[stack.length - 1]] < temperatures[i]
+    ) {
+      const index = stack.pop() as number;
       res[index] = i - index;
     }
     stack.push(i);
   }
   return res;
-};
+}
